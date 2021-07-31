07971ce3b8bcc0ccd104afbd7d52e0bc02d3b820
DandaraF <dandara-chr@hotmail.com>
2021-07-31 16:27:39 -0300

    WIP: Create header components

---
+++ src/assets/img/index.js
@@ -0,0 +1,8 @@
+import about from "./about.svg";
+import add from "./add.svg";
+import heart from "./heart.svg";
+import home from "./home.svg";
+import ongs from "./ongs.svg";
+import star from "./star.svg";
+
+export { about, add, heart, home, ongs, star };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Menu } from "../../components/Menu";
 
 const Home = () => {
-  return <div>home works</div>;
+  // return <div>home works</div>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Home;
