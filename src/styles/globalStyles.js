62b064acb89771fdcc582cd8dde77d7ba08a7bbb
DandaraF <dandara-chr@hotmail.com>
2021-08-03 15:53:49 -0300

    Remove margin e padding of html from page globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,12 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
-html{
-  width: 100vw;
-  margin: 0;
-  padding: 0;
-}
 body {
     margin: 0;
     padding: 0;
