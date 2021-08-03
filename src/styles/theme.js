812a8a77a09081c3530f0cf58c652866393f94d3
Gaby <lubaaenzin@gmail.com>
2021-08-02 21:10:41 -0300

    add theme provider and theme in project

---
+++ src/styles/theme.js
@@ -0,0 +1,43 @@
+const Theme = {
+  palette: {
+    primary: "#5A7770",
+    secondary: "#E39E19",
+    tertiary: {
+      tertiary_1: "#FF9899",
+      tertiary_2: "#FFB280",
+      tertiary_3: "#FFDA73",
+      tertiary_4: "#B7E17C",
+      tertiary_5: "#7CCED3",
+      tertiary_6: "#88C2F8",
+      tertiary_7: "#8C98FE",
+      tertiary_8: "#B898FA",
+    },
+    warning: "",
+    success: "",
+    text: {
+      primary: "#333333",
+      secondary: "#000000",
+      tertiary: "#373737",
+      placeholder: {
+        primary: "#CBD2D9",
+        secondary: "#9AA5B1",
+      },
+      light: {
+        primary: "#FFFFFF",
+        secondary: "#6A8376",
+      },
+      dark: {
+        primary: "#373737",
+        secondary: "#000000",
+        tertiary: "#616E7C",
+      },
+      info: "#E39E19",
+      default: "#333333",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+};
+
+export default Theme;
