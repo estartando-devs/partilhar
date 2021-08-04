0a259e1e307bced22b4eb377903c0048ad714160
iiizadora <oliveiras.izadora@gmail.com>
2021-08-04 20:53:14 -0300

    conflicts resolved

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,17 @@ import Input from "./Input";
 import SideMenu from "./SideMenu";
 import Search from "./Search";
 import Button from "./Button";
-<<<<<<< HEAD
 import Footer from "./Footer";
+import FilterCard from "./FilterCard";
 import LayoutComponent from "./LayoutComponent";
 
-export { Header, Input, SideMenu, Search, Button, Footer, LayoutComponent };
-=======
-import FilterCard from "./FilterCard";
-
-export { Header, Input, SideMenu, Search, Button, FilterCard };
->>>>>>> develop
+export {
+  Header,
+  Input,
+  SideMenu,
+  Search,
+  Button,
+  Footer,
+  LayoutComponent,
+  FilterCard,
+};
