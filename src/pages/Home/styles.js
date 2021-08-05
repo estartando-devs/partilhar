50bdcb8f32bdc77dec7631795beaa86a25f69d3a
DandaraF <dandara-chr@hotmail.com>
2021-08-05 13:39:40 -0300

    change style page home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* width: 100vw;
+  width: 100vw;
   height: 100vh;
-  background-color: red; */
-`;
-
-export const ContainerContent = styled.div`
-  /* display: flex;
-  flex-direction: column;
-  background-color: blue; */
 `;
