c0aeed7d5a896e8954ff81582c2bd78ff346acd2
iiizadora <oliveiras.izadora@gmail.com>
2021-08-09 22:19:16 -0300

    final adjustments to the card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,7 @@
 import { useHistory } from "react-router-dom";
 import * as S from "./styles";
-import * as I from "../../assets/img";
 
-const Card = () => {
+const Card = ({ title, text, img }) => {
   const history = useHistory();
 
   function handleClick() {
@@ -11,15 +10,10 @@ const Card = () => {
 
   return (
     <S.ContainerCard onClick={handleClick}>
-      <img src={I.ongcard} alt="Ong Wonder" />
+      <img src={img} alt={title} />
       <S.ContainerOngInfor>
-        <S.TextTitle>Ong Wonder</S.TextTitle>
-        <S.TextContents>
-          Somos uma ong especializada em cuidados com mulheres vítimas de abusos
-          psicológicosm onde de forma cultural e dinâmica as mesmas encaram seus
-          medos e adquirem novas metas e desejos para o futuro. Um lugar
-          construído com apoio e amor.
-        </S.TextContents>
+        <S.TextTitle>{title} </S.TextTitle>
+        <S.TextContents>{text}</S.TextContents>
         <S.TextLink>Ver mais...</S.TextLink>
       </S.ContainerOngInfor>
     </S.ContainerCard>
