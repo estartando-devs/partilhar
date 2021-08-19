7ad500a3a99494691a1eaf1fbfba01bba2c1de46
DandaraF <dandara-chr@hotmail.com>
2021-08-19 17:19:45 -0300

    resolve conflits

--- src/pages/Register/Steps/Cause/index.js
+++ src/pages/Register/Steps/Cause/index.js
@@ -1,22 +1,9 @@
-import { useState } from "react";
 import * as S from "./styles";
 import { filters } from "../../../../mocks/filterData";
 
 const Cause = ({ setColor }) => {
-  const [valueTextArea, setvalueTextArea] = useState("");
-
   function colorBottom(bgColor) {
     setColor(bgColor);
-  }
-  function limiterCaracter(e) {
-    setvalueTextArea({ text: e.target.value });
-    setvalueTextArea(e.target.value);
-    console.log(valueTextArea);
-    // const maxChars = 400;
-    // const inputLength = e.value.length;
-    // if (inputLength >= maxChars) {
-    //   console.log("maior");
-    // }
   }
 
   return (
@@ -41,11 +28,7 @@ const Cause = ({ setColor }) => {
           ))}
         </S.ContentCause>
         <S.LabelText>Faça um texto descritivo sobre a sua ONG</S.LabelText>
-        <S.TextArea
-          placeholder="Ex: data de fundação, objetivo, meta, histórico..."
-          value={valueTextArea}
-          onChange={(e) => limiterCaracter(e)}
-        />
+        <S.TextArea placeholder="Ex: data de fundação, objetivo, meta, histórico..." />
         <S.Counter>0/400</S.Counter>
       </S.Content>
     </S.Container>
