248bae2da44a44f873ae5b4d9d3723c6e474e32a
DandaraF <dandara-chr@hotmail.com>
2021-08-22 16:52:38 -0300

    WIP: creating chechebox

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Checkbox = () => {
+  return (
+    <S.Container>
+      <S.Checkbox type="checkbox" />
+    </S.Container>
+  );
+};
+
+export default Checkbox;
---
+++ src/components/Checkbox/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Checkbox = styled.input`
+  /* width: 24px;
+  height: 24px; */
+`;
